b2b0497101b88f0429906793f338a5221e39c8e1
Ronak Kumar <Ronakumar1211@gmail.com>
2026-07-08 23:49:19 +0530

    Remove light animation particles completely

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -29,15 +29,6 @@ export default function Home() {
       {/* Blueprint grid overlay */}
       <div className="fixed inset-0 blueprint-grid opacity-30 dark:opacity-10 pointer-events-none z-0" />
 
-      {/* Animated Blueprint Particles */}
-      {/* Going UP (First half of animation loop) */}
-      <div className="particle-v" style={{ left: '30%' }} />
-      <div className="particle-v" style={{ left: '70%' }} />
-      
-      {/* Going LEFT (Second half of animation loop) */}
-      <div className="particle-h" style={{ top: '30%' }} />
-      <div className="particle-h" style={{ top: '70%' }} />
-
       {/* Volumetric Glowing Orbs */}
       <div className="fixed top-1/4 left-1/4 w-96 h-96 bg-[#C17A45]/20 rounded-full blur-[120px] pointer-events-none z-0 animate-pulse" />
       <div className="fixed bottom-1/4 right-1/4 w-[30rem] h-[30rem] bg-[#4A5A6A]/20 rounded-full blur-[150px] pointer-events-none z-0 animate-pulse" style={{ animationDelay: '2s' }} />
