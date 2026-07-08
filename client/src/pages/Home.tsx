c33980d1846e13d4be96341d83592d8c7ce077bc
Ronak Kumar <Ronakumar1211@gmail.com>
2026-07-08 23:42:23 +0530

    Add animated blueprint light rays to background

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -29,6 +29,10 @@ export default function Home() {
       {/* Blueprint grid overlay */}
       <div className="fixed inset-0 blueprint-grid opacity-30 dark:opacity-10 pointer-events-none z-0" />
 
+      {/* Animated Blueprint Light Rays */}
+      <div className="light-ray-h opacity-30 dark:opacity-50" />
+      <div className="light-ray-v opacity-30 dark:opacity-50" />
+
       {/* Volumetric Glowing Orbs */}
       <div className="fixed top-1/4 left-1/4 w-96 h-96 bg-[#C17A45]/20 rounded-full blur-[120px] pointer-events-none z-0 animate-pulse" />
       <div className="fixed bottom-1/4 right-1/4 w-[30rem] h-[30rem] bg-[#4A5A6A]/20 rounded-full blur-[150px] pointer-events-none z-0 animate-pulse" style={{ animationDelay: '2s' }} />
