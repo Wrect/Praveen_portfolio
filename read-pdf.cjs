3713965bd3938912fc23b7a17dc2c8b54fd5c465
Ronak Kumar <Ronakumar1211@gmail.com>
2026-07-08 21:34:11 +0530

    Clean up unnecessary and duplicate files

--- read-pdf.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('C:/Users/Rahul/Desktop/gh/client/public/resume.pdf');
-pdf(dataBuffer).then(function(data) {
-  console.log(data.text);
-}).catch(console.error);
